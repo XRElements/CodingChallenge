f1d82ac8669dfb0c9a95c6b96e8399123d292b3b
XRElements <98510218+XRElements@users.noreply.github.com>
2022-01-28 09:00:42 +1100

    Fixed up for presentation

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XRE.Common.Models;
+using XRE.Common.Functions;
+using XRE.Common.Functions.CRUD;
+
+namespace XRE_Grad_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<BookingModel> Bookings = CRUD_Functions.SetupBookings();
+            List<DayModel> Days = CRUD_Functions.SetupDays(Bookings);
+            List<ActivityProviderModel> ActivityProviders = CRUD_Functions.SetupProviders();
+            //List of activities here
+        }
+    }
+}
